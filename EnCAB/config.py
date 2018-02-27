0bfaec0ccb4921e28ad42fd395a5d06c7b5bd7f5
fornib <b@forni.me>
2018-02-27 18:48:51 +0100

    configuration

---
+++ EnCAB/config.py
@@ -0,0 +1,20 @@
+""" configuration file for EnCAB.py """
+
+
+# All paths can be absolute or relative to the program file
+# Use ONLY the slash " / " as directory separator
+
+
+# Path to root directory where the website is located
+# the program  will scan all .html or .htm files in directory and subdirectories
+WEBSITE_DIR = "test"  # default: "../docs"
+
+# Path to directory where algorithm data are located (various .xml or .txt files)
+ALGORITHMS_DIR = "algorithms-test"  # default: "algorithms"
+
+# Path to directory where templates are located
+TEMPLATE_DIR = "template"  # default: "template"
+
+# Templates file names
+TEMPLATE_BLOCK = "block.html"  # default: "block.html"
+TEMPLATE_INDEX = "index.html"  # default: "index.html"
